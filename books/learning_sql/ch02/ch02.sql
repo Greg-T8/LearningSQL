a02cb7e4d35444c702cc4b01ded2eb47d9cb2dcc
Greg Tate <gregt8@gmail.com>
2025-04-13 03:43:25 -0500

    Chapter 02: Add notes for basic MySQL commands

---
+++ books/learning_sql/ch02/ch02.sql
@@ -0,0 +1,9 @@
+-- #########################################
+-- MySQL Learning Notes - Chapter 02
+-- Book: Learning SQL by Alan Beaulieu
+-- #########################################
+
+-- 1. Basic MySQL Commands
+mysql -u <username> -p              -- login to mysql
+mysql -u <username> -p <database>   -- login to mysql and use a specific
+
