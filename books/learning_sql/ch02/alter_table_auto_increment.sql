164b0c66b2dbab5ee7be97e17939544381cefb49
Greg Tate <gregt8@gmail.com>
2025-05-06 02:56:23 -0500

    Ch02: Add SQL script to alter person_id to AUTO_INCREMENT in person table

---
+++ books/learning_sql/ch02/alter_table_auto_increment.sql
@@ -0,0 +1,11 @@
+/*
+Program: Alter person_id to AUTO_INCREMENT in person table
+Context: Learning SQL, Chapter 2 (Book exercise)
+Author: Greg Tate
+Date: 2025-05-06
+*/
+
+set foreign_key_checks = 0;
+ALTER TABLE person
+    MODIFY person_id SMALLINT UNSIGNED AUTO_INCREMENT;
+set foreign_key_checks = 1;
